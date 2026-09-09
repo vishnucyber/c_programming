f7f5397ecee524adf49d1b0f7cc45f656b8e4e56
Vishnu_cyber <vishnucyber19@gmail.com>
2026-09-09 21:15:40 +0530

    Add files via upload

---
+++ circ_calc.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+int  main(void){
+    double  radius  = 0.0;
+    double area = 0.0;
+    double surface_area = 0.0;
+    double volume = 0.0;
+
+    const double PI = 3.141519;
+
+    printf("Enter the radius: ");
+    scanf("%lf",&radius);
+
+    area = PI * pow(radius,2);
+    surface_area = 4 * PI * pow(radius,2);
+    volume = 4 * PI * pow(radius,3);
+    printf("\nArea: %lf\n",area);
+    printf("Surface Area: %lf\n",surface_area);
+    printf("Volume: %lf\n",volume);
+
+   return 0;
+
+}
+
+
